1b6f12b15d7d1e3800db47ca045af5883965abd3
Ryan Ramos <ryanramoss153@gmail.com>
2026-04-29 22:47:17 -0300

    Subindo exercícios de condiconal

---
+++ exercicios/01_Condicional/exercicio_01.ts
@@ -0,0 +1,11 @@
+// 1. Crie um programa que leia um número e informe se ele é:
+// a) Par ou Ímpar
+// b) Positivo ou Negativo
+
+export function exercicio_01(): void {
+    let numero: number
+    numero = Number(prompt(`Informe um número para confirmar se o número é par ou impar, negativo: `))
+
+    console.log(numero % 2 == 0 ? "Par" : "Impar")
+    console.log(numero == 0 ? "Zero" : numero > 0 ? "Positivo" : "Negativo")
+}
---
+++ exercicios/01_Condicional/exercicio_04.ts
@@ -0,0 +1,11 @@
+// Ler dois valores e imprimir uma das três mensagens a seguir:
+// ● ‘Números iguais’, caso os números sejam iguais;
+// ● ‘Primeiro é maior’, caso o primeiro seja maior que o segundo;
+// ● ‘Segundo maior’, caso o segundo seja maior que o primeiro.
+
+export function exercicio_04(): void{
+    let numero_1:number = 0, numero_2:number = 0
+    numero_1 = Number(prompt(`Informe o primeiro número: `))
+    numero_2 = Number(prompt(`Informe o segundo número: `))
+    console.log(numero_1 == numero_2 ? `Primeiro número é igual ao segundo número digitado` : numero_1 > numero_2 ? `Primeiro número é maior que segundo número digitado`: `Segundo número é maior que o primeiro número digitado`)
+}
